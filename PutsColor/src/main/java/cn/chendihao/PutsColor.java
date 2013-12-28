5991c284513f7b5aa554a2e2abd952c9be49abcc
tobe <tobegit3hub@gmail.com>
2013-12-28 13:50:53 +0800

    indent src and test code

--- PutsColor/src/main/java/cn/chendihao/PutsColor.java
+++ PutsColor/src/main/java/cn/chendihao/PutsColor.java
@@ -3,35 +3,34 @@ package cn.chendihao;
 /**
  * PutsColor
  */
-public class PutsColor 
-{
+public class PutsColor {
 
-	public static void puts_red(String string){
-		System.out.println("\033[0;31m" + string + "\033[0m");
-	}
+  public static void puts_red(String string){
+    System.out.println("\033[0;31m" + string + "\033[0m");
+  }
 
-	public static void puts_green(String string){
-		System.out.println("\033[0;32m" + string + "\033[0m");
-	}
+  public static void puts_green(String string){
+    System.out.println("\033[0;32m" + string + "\033[0m");
+  }
 
-	public static void puts_yellow(String string){
-		System.out.println("\033[0;33m" + string + "\033[0m");
-	}
+  public static void puts_yellow(String string){
+    System.out.println("\033[0;33m" + string + "\033[0m");
+  }
 
-	public static void puts_blue(String string){
-		System.out.println("\033[0;34m" + string + "\033[0m");
-	}
+  public static void puts_blue(String string){
+    System.out.println("\033[0;34m" + string + "\033[0m");
+  }
 
-	public static void puts_purple(String string){
-		System.out.println("\033[0;35m" + string + "\033[0m");
-	}
+  public static void puts_purple(String string){
+    System.out.println("\033[0;35m" + string + "\033[0m");
+  }
 
-	public static void puts_cyan(String string){
-		System.out.println("\033[0;36m" + string + "\033[0m");
-	}
-
-	public static void puts_grey(String string){
-		System.out.println("\033[0;37m" + string + "\033[0m");
-	}
+  public static void puts_cyan(String string){
+    System.out.println("\033[0;36m" + string + "\033[0m");
+  }
+  
+  public static void puts_grey(String string){
+    System.out.println("\033[0;37m" + string + "\033[0m");
+  }
 
 }
--- PutsColor/src/test/java/cn/chendihao/TestPutsColor.java
+++ PutsColor/src/test/java/cn/chendihao/TestPutsColor.java
@@ -6,39 +6,39 @@ import org.junit.Test;
 
 public class TestPutsColor extends TestCase {
 
-	@Test
-	public static void testPutsRed(){
-		PutsColor.puts_red("Hello, I'm red.");
-	}
+  @Test
+  public static void testPutsRed(){
+    PutsColor.puts_red("Hello, I'm red.");
+  }
 
-	@Test
-	public static void testPutsGreen(){
-		PutsColor.puts_green("What's wrong with green?");
-	}
+  @Test
+  public static void testPutsGreen(){
+    PutsColor.puts_green("What's wrong with green?");
+  }
 
-	@Test
-	public static void testPutsYellow(){
-		PutsColor.puts_yellow("I'm innocent yellow.");
-	}
+  @Test
+  public static void testPutsYellow(){
+    PutsColor.puts_yellow("I'm innocent yellow.");
+  }
 
-	@Test
-	public static void testPutsBlue(){
-		PutsColor.puts_blue("I know because it's blue.");
-	}
+  @Test
+  public static void testPutsBlue(){
+    PutsColor.puts_blue("I know because it's blue.");
+  }
 
-	@Test
-	public static void testPutsPurple(){
-		PutsColor.puts_purple("Don't forget the purple");
-	}
+  @Test
+  public static void testPutsPurple(){
+    PutsColor.puts_purple("Don't forget the purple");
+  }
 
-	@Test
-	public static void testPutsCyan(){
-		PutsColor.puts_cyan("Cyan is hard to remember.");
-	}
+  @Test
+  public static void testPutsCyan(){
+    PutsColor.puts_cyan("Cyan is hard to remember.");
+  }
 
-	@Test
-	public static void testPutsGrey(){
-		PutsColor.puts_grey("Actually grep is the last one.");
-	}
+  @Test
+  public static void testPutsGrey(){
+    PutsColor.puts_grey("Actually grep is the last one.");
+  }
 
 }
