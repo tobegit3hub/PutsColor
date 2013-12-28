ff12ce36f3f5330dc1888c9f86c403f4e344b1da
tobe <tobegit3hub@gmail.com>
2013-12-28 12:34:11 +0800

    add code and test of PutsColor

---
+++ PutsColor/src/test/java/cn/chendihao/TestPutsColor.java
@@ -0,0 +1,44 @@
+package cn.chendihao;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class TestPutsColor extends TestCase {
+
+	@Test
+	public static void testPutsRed(){
+		PutsColor.puts_red("Hello, I'm red.");
+	}
+
+	@Test
+	public static void testPutsGreen(){
+		PutsColor.puts_green("What's wrong with green?");
+	}
+
+	@Test
+	public static void testPutsYellow(){
+		PutsColor.puts_yellow("I'm innocent yellow.");
+	}
+
+	@Test
+	public static void testPutsBlue(){
+		PutsColor.puts_blue("I know because it's blue.");
+	}
+
+	@Test
+	public static void testPutsPurple(){
+		PutsColor.puts_purple("Don't forget the purple");
+	}
+
+	@Test
+	public static void testPutsCyan(){
+		PutsColor.puts_cyan("Cyan is hard to remember.");
+	}
+
+	@Test
+	public static void testPutsGrey(){
+		PutsColor.puts_grey("Actually grep is the last one.");
+	}
+
+}
